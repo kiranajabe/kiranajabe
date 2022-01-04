b0a9d25902e5e852b4c0f09c2dd9421ac116029e
kiranajabe <97108661+kiranajabe@users.noreply.github.com>
2022-01-04 19:58:38 +0530

    Add files via upload

---
+++ avd.bat
@@ -0,0 +1,2 @@
+C:\Users\kiran.ajabe\AppData\Local\Android\Sdk\emulator\emulator.exe -avd hcp3 -selinux permissive -writable-system -no-snapshot -verbose -qemu -drive format=raw,file="C:\Users\kiran.ajabe\AppData\Local\Android\Sdk\system-images\android-30\hcp3-audi-automotive\x86_64\ext-storage.raw\ext-storage.raw"
+
